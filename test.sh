6f657748e27848dc1ea63acc36a45c2b057d87f4
mRSun15 <3150105645@zju.edu.cn>
2019-04-28 00:53:06 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ python3 run_classifier.py \
   --task_name=CoLA \
   --do_train=true \
   --do_eval=true \
-  --data_dir=$DATA_DIR \
+  --data_dir=$DATA_DIR/CoLA \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
